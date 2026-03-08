de5c482e2c25dd03e02b79e3231d63e66d3a68fe
Dinand <s1188615@student.windesheim.nl>
2026-03-08 21:48:52 +0100

    Ingredient aangepast

--- ingredient.py
+++ ingredient.py
@@ -7,12 +7,33 @@ class Ingredient:
         self.__kcal = kcal
         self.__plantaardig_alternatief = plantaardig_alternatief
 
-    def __str__(self) -> str:
-        # Mooier weergeven: 1.0 -> 1
-        if self.hoeveelheid == int(self.hoeveelheid):
-            hoeveelheid_tekst = str(int(self.hoeveelheid))
-        else:
-            hoeveelheid_tekst = str(self.hoeveelheid)
+    def get_naam(self):
+        return self.__naam
 
-        return f"{hoeveelheid_tekst} {self.eenheid} {self.naam}"
-        
+    def get_hoeveelheid(self):
+        return self.__hoeveelheid
+
+    def get_eenheid(self):
+        return self.__eenheid
+
+    def get_kcal(self):
+        return self.__kcal
+
+    def get_plantaardig_alternatief(self):
+        return self.__plantaardig_alternatief
+
+    def set_hoeveelheid(self, aantal_personen):
+        self.__hoeveelheid = self.__basis_hoeveelheid * aantal_personen
+
+    def get_ingredient(self, plantaardig):
+        if plantaardig and self.__plantaardig_alternatief is not None:
+            return self.__plantaardig_alternatief
+        return self
+
+    def __str__(self):
+        if self.__hoeveelheid == int(self.__hoeveelheid):
+            hoeveelheid_tekst = str(int(self.__hoeveelheid))
+        else:
+            hoeveelheid_tekst = str(self.__hoeveelheid)
+
+        return f"{hoeveelheid_tekst} {self.__eenheid} {self.__naam} ({self.__kcal} kcal)"
