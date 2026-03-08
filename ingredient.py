7a8c1b6eb3bb95b8892bcbb1d99697cb57d3bc2a
Dinand <s1188615@student.windesheim.nl>
2026-03-08 21:22:06 +0100

    Voeg kcal en plantaardig alternatief toe aan Ingredient klasse

--- ingredient.py
+++ ingredient.py
@@ -1,8 +1,11 @@
 class Ingredient:
-    def __init__(self, naam: str, hoeveelheid: float, eenheid: str):
-        self.naam = naam
-        self.hoeveelheid = hoeveelheid
-        self.eenheid = eenheid
+    def __init__(self, naam, hoeveelheid, eenheid, kcal, plantaardig_alternatief=None):
+        self.__naam = naam
+        self.__hoeveelheid = hoeveelheid
+        self.__basis_hoeveelheid = hoeveelheid
+        self.__eenheid = eenheid
+        self.__kcal = kcal
+        self.__plantaardig_alternatief = plantaardig_alternatief
 
     def __str__(self) -> str:
         # Mooier weergeven: 1.0 -> 1
@@ -12,4 +15,4 @@ class Ingredient:
             hoeveelheid_tekst = str(self.hoeveelheid)
 
         return f"{hoeveelheid_tekst} {self.eenheid} {self.naam}"
-    
+        
